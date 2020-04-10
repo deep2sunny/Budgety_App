34889283867d1f61771d7da2aace83ef5c7ecb5b
Akashdeep Singh <deep2sunny@gmail.com>
2020-04-10 15:40:40 -0400

    Setup Controllers

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+//BUDGET CONTROLLER
+var budgetController = (function(){
+
+
+
+})();
+
+
+//UI CONTROLLER
+var UIController = (function(){
+
+})();
+
+
+//GOLBAL APP CONTROLLER
+var controllerApp = (function(budgetCtrl, UICtrl){
+
+
+
+})(budgetController, UIController);
