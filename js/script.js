421fb0033bf02e46329a148b600093d5a6013c98
Akashdeep Singh <deep2sunny@gmail.com>
2020-05-14 01:48:04 -0400

    Finishing touches to improve UX

--- js/script.js
+++ js/script.js
@@ -201,6 +201,13 @@ var UIController = (function(){
     return sign + ' ' + int + '.' + dec;
   }
 
+  var nodeListForEach = function(list, callback){
+     for(var i=0; i<list.length; i++){
+       console.log("Fields = " + list[i]);
+       callback(list[i],i);
+     }
+  };
+
   return {
     getInput: function(){
         return {
@@ -280,14 +287,6 @@ var UIController = (function(){
 
         var fields = document.querySelectorAll(DOMstrings.expensePercLabel);
 
-
-        var nodeListForEach = function(list, callback){
-           for(var i=0; i<list.length; i++){
-             console.log("Fields = " + list[i]);
-             callback(list[i],i);
-           }
-        };
-
         nodeListForEach(fields, function(current, index){
           if(percentages[index] > 0){
             current.textContent = percentages[index] + '%';
@@ -305,7 +304,19 @@ var UIController = (function(){
         month = now.getMonth();
         year = now.getFullYear();
         document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+      },
 
+      changeType: function(){
+
+        var fields = document.querySelectorAll(
+          DOMstrings.inputType + ',' +
+          DOMstrings.inputDescription + ',' +
+          DOMstrings.inputValue);
+
+        nodeListForEach(fields, function(cur){
+          cur.classList.toggle('red-focus');
+        });
+        document.querySelector(DOMstrings.inputButton).classList.toggle('red');
       }
   };
 
@@ -331,6 +342,8 @@ var controllerApp = (function(budgetCtrl, UICtrl){
     });
 
     document.querySelector(DOM.container).addEventListener('click',ctrlDeleteItem);
+
+    document.querySelector(DOM.inputType).addEventListener('change',UICtrl.changeType);
 
   };
 
